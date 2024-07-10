e1c5bba962273848542c8075f77202dab8fa6888
Juan Fonseca <juanpablofonsecac5@gmail.com>
2024-07-10 17:11:34 -0500

    fix por espacios

--- scripts/kill-current-process.sh
+++ scripts/kill-current-process.sh
@@ -2,9 +2,9 @@
 
 PID=$(lsof -t -i :8080)
 
-if [-n "$PID"]; then
+if [ -n "$PID" ]; then
     kill $PID
     echo "Proceso con PID $PID detenido."
 else
-    echo "No se encontro ningun proceso en el puerto 8080."
+    echo "No se encontró ningún proceso en el puerto 8080."
 fi

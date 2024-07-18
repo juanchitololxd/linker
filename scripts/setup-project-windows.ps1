0061019f5fb74aef27f92c1886e3f2c6702b10c3
Juan Fonseca <juanpablofonsecac5@gmail.com>
2024-07-18 07:43:04 -0500

    scripts para variables de entorno done

---
+++ scripts/setup-project-windows.ps1
@@ -0,0 +1,13 @@
+param (
+    [string]$RUN_ENV = "dev"
+)
+
+if ($RUN_ENV -eq "dev") {
+    Write-Output "Running in dev mode"
+    Copy-Item -Path "environments/.env.dev" -Destination ".env" -Force
+    Copy-Item -Path "environments/.env.dev" -Destination "./cmd/api/application/.env" -Force
+} else {
+    Write-Output "Running in prod mode"
+    Copy-Item -Path "environments/.env.prod" -Destination ".env" -Force
+    Copy-Item -Path "environments/.env.prod" -Destination "./cmd/api/application/.env" -Force
+}

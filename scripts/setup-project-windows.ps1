131d263b55481c134c13a017f9e9a43fc824aa8c
Juan Fonseca <juanpablofonsecac5@gmail.com>
2024-07-23 07:08:54 -0500

    variables de entorno prod

--- scripts/setup-project-windows.ps1
+++ scripts/setup-project-windows.ps1
@@ -6,8 +6,10 @@ if ($RUN_ENV -eq "dev") {
     Write-Output "Running in dev mode"
     Copy-Item -Path "environments/.env.dev" -Destination ".env" -Force
     Copy-Item -Path "environments/.env.dev" -Destination "./cmd/api/application/.env" -Force
+    Copy-Item -Path "environments/.env.dev" -Destination "./cmd/api/services/.env" -Force
 } else {
     Write-Output "Running in prod mode"
     Copy-Item -Path "environments/.env.prod" -Destination ".env" -Force
     Copy-Item -Path "environments/.env.prod" -Destination "./cmd/api/application/.env" -Force
+    Copy-Item -Path "environments/.env.prod" -Destination "./cmd/api/services/.env" -Force
 }
